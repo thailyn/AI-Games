e4b1385095ebe1374ab02170aca64cb0c2ffc8a7
Charles Macanka <cmacanka@gmail.com>
2014-05-27 19:37:55 -0400

    Added a console observer class.
    This class is a TicTacToePlayer but is specifically designed to print
    out the current state after each move, so the observer can watch the
    game as it progresses.

---
+++ TicTacToe/Game/ConsoleObserver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Engine;
+
+namespace TicTacToe.Game
+{
+    public class ConsoleObserver : TicTacToePlayer
+    {
+        public ConsoleObserver(TicTacToeOptions options)
+        {
+            Symbol = Game.Symbol.Blank;
+            Options = options;
+
+            CurrentState = new TicTacToeState(Options);
+        }
+
+        public override void UpdateState(List<TicTacToeMove> moves)
+        {
+            foreach (var move in moves)
+            {
+                if (move == null)
+                {
+                    continue;
+                }
+
+                CurrentState = CurrentState.ApplyMove(move);
+
+                PrintState(CurrentState);
+                System.Console.ReadLine();
+            }
+        }
+
+        public override TicTacToeMove GetMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
