96c3f2f5809dc39bceea604b6ecb6a9c97e48543
Charles Macanka <cmacanka@gmail.com>
2014-05-28 09:56:35 -0400

    Moved minimax algorithm into its own class.
    Now this algorithm can be reused in multiple games without needing to
    write it from scratch each time.

---
+++ Engine/Algorithms/BasicMinimaxSearch.cs
@@ -0,0 +1,86 @@
+﻿using Engine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Algorithms
+{
+    public class BasicMinimaxSearch<T, U, V, W>
+        where T : IGameOptions<U, W, T, V>
+        where U : IPlayer<W, T, V, U>
+        where V : IMove<W, T, U, V>
+        where W : IState<T, U, V, W>
+    {
+        public V Search(W initialState, Func<W, IEnumerable<V>> getAvailableMovesFunc, U searchingPlayer, int depth, bool maximize)
+        {
+            List<V> availableMoves = getAvailableMovesFunc(initialState).ToList();
+            int bestMoveValue = int.MinValue;
+            V bestMove = default(V);
+
+            foreach (var move in availableMoves)
+            {
+                int moveValue = MinimaxSearchStep(initialState.ApplyMove(move), getAvailableMovesFunc, searchingPlayer, false);
+                if (moveValue > bestMoveValue)
+                {
+                    bestMoveValue = moveValue;
+                    bestMove = move;
+                }
+            }
+
+            return bestMove;
+        }
+
+        protected int MinimaxSearchStep(W state, Func<W, IEnumerable<V>> getAvailableMovesFunc, U searchingPlayer, bool maximize)
+        {
+            List<U> winningPlayers = null;
+            if (state.IsEndState(out winningPlayers))
+            {
+                if (winningPlayers.Count == 0 || winningPlayers.Count > 1)
+                {
+                    return 0;
+                }
+
+                var winningPlayer = winningPlayers[0];
+                if (searchingPlayer.Equals(winningPlayer))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            if (maximize)
+            {
+                int bestValue = int.MinValue;
+                List<V> availableMoves = getAvailableMovesFunc(state).ToList();
+                foreach (var move in availableMoves)
+                {
+                    W nextState = state.ApplyMove(move);
+                    int val = MinimaxSearchStep(nextState, getAvailableMovesFunc,
+                        searchingPlayer, searchingPlayer.Equals(nextState.CurrentPlayer));
+                    bestValue = val > bestValue ? val : bestValue;
+                }
+
+                return bestValue;
+            }
+            else
+            {
+                int bestValue = int.MaxValue;
+                List<V> availableMoves = getAvailableMovesFunc(state).ToList();
+                foreach (var move in availableMoves)
+                {
+                    W nextState = state.ApplyMove(move);
+                    int val = MinimaxSearchStep(nextState, getAvailableMovesFunc,
+                        searchingPlayer, searchingPlayer.Equals(nextState.CurrentPlayer));
+                    bestValue = val < bestValue ? val : bestValue;
+                }
+
+                return bestValue;
+            }
+        }
+    }
+}
--- TicTacToe/Game/BasicMinimaxPlayer.cs
+++ TicTacToe/Game/BasicMinimaxPlayer.cs
@@ -32,76 +32,10 @@ namespace TicTacToe.Game
 
         public override TicTacToeMove GetMove()
         {
-            var bestMove = MinimaxSearch(CurrentState, true);
+            var basicMinimax = new Engine.Algorithms.BasicMinimaxSearch<TicTacToeOptions, TicTacToePlayer,
+                TicTacToeMove, TicTacToeState>();
+            var bestMove = basicMinimax.Search(CurrentState, GetAvailableMoves, this, -1, true);
             return bestMove;
-        }
-
-        protected TicTacToeMove MinimaxSearch(TicTacToeState state, bool maximize)
-        {
-            List<TicTacToeMove> availableMoves = GetAvailableMoves(state).ToList();
-            int bestMoveValue = int.MinValue;
-            TicTacToeMove bestMove = null;
-
-            foreach (var move in availableMoves)
-            {
-                int moveValue = MinimaxSearchStep(state.ApplyMove(move), false);
-                if (moveValue > bestMoveValue)
-                {
-                    bestMoveValue = moveValue;
-                    bestMove = move;
-                }
-            }
-
-            return bestMove;
-        }
-
-        protected int MinimaxSearchStep(TicTacToeState state, bool maximize)
-        {
-            List<TicTacToePlayer> winningPlayers = null;
-            if (state.IsEndState(out winningPlayers))
-            {
-                if (winningPlayers.Count == 0 || winningPlayers.Count > 1)
-                {
-                    return 0;
-                }
-
-                var winningPlayer = winningPlayers[0];
-                if (winningPlayer == this)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return -1;
-                }
-            }
-
-            if (maximize)
-            {
-                int bestValue = int.MinValue;
-                List<TicTacToeMove> availableMoves = GetAvailableMoves(state).ToList();
-                foreach (var move in availableMoves)
-                {
-                    TicTacToeState nextState = state.ApplyMove(move);
-                    int val = MinimaxSearchStep(nextState, nextState.CurrentPlayer == this);
-                    bestValue = val > bestValue ? val : bestValue;
-                }
-
-                return bestValue;
-            }
-            else
-            {
-                int bestValue = int.MaxValue;
-                List<TicTacToeMove> availableMoves = GetAvailableMoves(state).ToList();
-                foreach (var move in availableMoves)
-                {
-                    TicTacToeState nextState = state.ApplyMove(move);
-                    int val = MinimaxSearchStep(nextState, nextState.CurrentPlayer == this);
-                    bestValue = val < bestValue ? val : bestValue;
-                }
-
-                return bestValue;
-            }
         }
 
         protected IEnumerable<TicTacToeMove> GetAvailableMoves(TicTacToeState currentState)
