8305be2afa90537ef1a7c8582c552b72ac993ea6
Charles Macanka <cmacanka@gmail.com>
2014-05-31 16:03:36 -0400

    Implemented method to get move from human player.

--- Checkers/Game/HumanConsolePlayer.cs
+++ Checkers/Game/HumanConsolePlayer.cs
@@ -51,7 +51,82 @@ namespace Checkers.Game
 
         public override CheckersMove GetMove()
         {
-            throw new NotImplementedException();
+            if (CurrentState == null)
+            {
+                Console.WriteLine("The current state is unknown.");
+            }
+            else
+            {
+                Console.WriteLine("This is the current state:");
+                PrintState(CurrentState);
+
+                System.Console.WriteLine();
+            }
+
+            // TODO: Error checking and making sure the entered locations are valid.
+
+            int pieceRow = -1;
+            int pieceColumn = -1;
+
+            CheckersMove previousMove = null;
+            Moves.JumpPieceMove previousJumpMove = null;
+            if (CurrentState.MovesThisTurn.Count > 0)
+            {
+                previousMove = CurrentState.MovesThisTurn[CurrentState.MovesThisTurn.Count - 1];
+            }
+
+            if (previousMove is Moves.JumpPieceMove)
+            {
+                previousJumpMove = previousMove as Moves.JumpPieceMove;
+                pieceRow = previousJumpMove.EndRow;
+                pieceColumn = previousJumpMove.EndColumn;
+
+                System.Console.WriteLine("You must either jump again with the piece located at {0}, {1}, or enter nothing to end your turn.",
+                    pieceRow, pieceColumn);
+            }
+            else
+            {
+                System.Console.Write("Please enter the row of the piece to move: ");
+                pieceRow = System.Console.ReadLine()[0] - Convert.ToInt32('0');
+
+                System.Console.Write("Please enter the column of the piece to move: ");
+                pieceColumn = System.Console.ReadLine()[0] - Convert.ToInt32('0');
+            }
+
+            System.Console.Write("Please enter the row of the location to move to: ");
+            var lineEntered = System.Console.ReadLine();
+            if (lineEntered.Length == 0 && previousJumpMove != null)
+            {
+                return new Moves.EndTurnMove(this);
+            }
+
+            char firstCharacterEntered = lineEntered[0];
+            int destinationRow = firstCharacterEntered - Convert.ToInt32('0');
+
+            System.Console.Write("Please enter the column of the location to move to: ");
+            int destinationColumn = System.Console.ReadLine()[0] - Convert.ToInt32('0');
+
+            int rowDelta = Math.Abs(pieceRow - destinationRow);
+            Piece movingPiece = CurrentState.Board[pieceRow * CurrentState.Options.NumColumns + pieceColumn].Piece;
+            if (rowDelta == 1 && previousJumpMove == null)
+            {
+                return new Moves.SlidePieceMove(pieceRow, pieceColumn, destinationRow,
+                    destinationColumn, movingPiece, this);
+            }
+            else if (rowDelta == 2)
+            {
+                int opponentPieceRow = pieceRow + ((destinationRow - pieceRow) / 2);
+                int opponentPieceColumn = pieceColumn + ((destinationColumn - pieceColumn) / 2);
+                Piece jumpedPiece = CurrentState.Board[opponentPieceRow * CurrentState.Options.NumColumns + opponentPieceColumn].Piece;
+
+                return new Moves.JumpPieceMove(pieceRow, pieceColumn, destinationRow,
+                    destinationColumn, movingPiece, jumpedPiece, this);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
         }
 
         public void PrintCurrentState()
