f6c1a7a5114c434f6d67cde7e07e334a741f4393
Charles Macanka <cmacanka@gmail.com>
2014-05-31 21:00:38 -0400

    Added ConsoleObserver class.

---
+++ Checkers/Game/Players/ConsoleObserver.cs
@@ -0,0 +1,42 @@
+﻿using Checkers.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Game.Players
+{
+    public class ConsoleObserver : CheckersPlayer
+    {
+        public ConsoleObserver(CheckersOptions options)
+        {
+            Team = Game.Team.None;
+            Options = options;
+        }
+
+        public override void UpdateState(List<CheckersMove> moves)
+        {
+            foreach (var move in moves)
+            {
+                if (move == null)
+                {
+                    continue;
+                }
+
+                CurrentState = CurrentState.ApplyMove(move);
+            }
+            
+            if (moves.Any(m => m is Game.Moves.EndTurnMove))
+            {
+                PrintState(CurrentState);
+                System.Console.ReadLine();
+            }
+        }
+
+        public override CheckersMove GetMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
